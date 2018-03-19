aad59d68d804e579902e205e9e6bf2a0070785c3
yanghaotai <yanghaotai@163.com>
2018-03-19 23:25:10 +0800

    搜索

--- guest/urls.py
+++ guest/urls.py
@@ -24,4 +24,7 @@ urlpatterns = [
 	url(r'^account/login/$', views.index),
 	url(r'^login_action/$', views.login_action),
 	url(r'^event_manage/$', views.event_manage),
+	url(r'^search_name/$', views.search_name),
+	url(r'^guest_manage/$', views.guest_manage),
+	url(r'^search_realname/$', views.search_realname),
 ]
--- sign/views.py
+++ sign/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse,HttpResponseRedirect
 from django.contrib import auth
 from django.contrib.auth.decorators import login_required
+from sign.models import Event,Guest
 
 # Create your views here.
 def index(request):
@@ -27,6 +28,35 @@ def login_action(request):
 #发布会管理
 @login_required
 def event_manage(request):
+	event_list = Event.objects.all()
 	username = request.COOKIES.get('user','')  #读取浏览器cookie
 	username = request.session.get('user','') #读取浏览器session
-	return render(request,"event_manage.html",{"user":username})
+	return render(request,"event_manage.html",{"user":username,
+											   "events":event_list})
+
+#发布会名称搜索
+@login_required
+def search_name(request):
+	username = request.session.get('user','') #读取浏览器session
+	search_name = request.GET.get("name","")
+	event_list = Event.objects.filter(name__contains=search_name)
+	return render(request,"event_manage.html",{"user":username,
+											   "events":event_list})
+
+#嘉宾管理
+@login_required
+def guest_manage(request):
+	guest_list = Guest.objects.all()
+	username = request.session.get('user','') #读取浏览器session
+	return render(request,"guest_manage.html",{"user":username,
+											   "guests":guest_list})
+
+
+#嘉宾姓名搜索
+@login_required
+def search_realname(request):
+	username = request.session.get('user','') #读取浏览器session
+	search_realname = request.GET.get("phone","")
+	guest_list = Guest.objects.filter(phone__contains=search_realname)
+	return render(request,"guest_manage.html",{"user":username,
+											   "guests":guest_list})
